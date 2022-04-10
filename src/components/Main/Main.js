7b95a57a14f784a3b2c21ec1dbb4aa9768815a81
anamerasul <anamerasul@gmail.com>
2022-04-11 01:19:38 +0600

    Use Effect with dependency List

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -15,9 +15,23 @@ const Main = () => {
     //         .then(data => setVolunteers(data))
     // }, [])
 
-    const [volunteers] = useVolunteers([])
+    // const [volunteers] = useVolunteers([])
 
     const [searchResult, setSearchResult] = useState([])
+
+    const [searchText, setSearchText] = useState('')
+
+    useEffect(() => {
+
+        console.log('inside the use effect')
+        const url = `data.json`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                const match = data.filter(d => d.title.includes(searchText))
+                setSearchResult(match)
+            })
+    }, [searchText])
 
     // const [volunteers] = useVolunteers();
     // const [searchText, setSearchText] = useState('');
@@ -44,13 +58,22 @@ const Main = () => {
     // }\
 
     const handleSearchChange = event => {
-        console.log(event.target.value)
+        // console.log(event.target.value)
 
-        const searchText = event.target.value
-        const match = volunteers.filter(v => v.title.includes(searchText))
-        setSearchResult(match)
+        // const searchText = event.target.value
+        // const match = volunteers.filter(v => v.title.includes(searchText))
+        // setSearchResult(match)
+        setSearchText(event.target.value)
 
     }
+    // const handleSearchChange2 = event => {
+    //     console.log(event.target.value)
+
+    //     const searchText = event.target.value
+    //     const match = volunteers.filter(v => v.title.includes(searchText))
+    //     setSearchResult(match)
+
+    // }
 
 
 
@@ -68,7 +91,7 @@ const Main = () => {
                     ></Activity>)
                 } */}
             {/* </div> */}
-            <h2>Volunteer Activities: {volunteers.length}</h2>
+            <h2>Volunteer Activities: {searchResult.length}</h2>
             <div style={{ 'margin': '20px' }}>
                 <input onChange={handleSearchChange} type="text" placeholder='search' />
             </div>
